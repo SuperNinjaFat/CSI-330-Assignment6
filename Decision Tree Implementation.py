f88a356f43e09aa3035ae865bef32aca1391aeaa
tynanpmatthews <tynanpmatthews@gmail.com>
2019-11-07 14:54:33 -0500

    Add question class

--- Decision Tree Implementation.py
+++ Decision Tree Implementation.py
@@ -1,8 +1,8 @@
 import csv
 import time
 import operator
+import numbers
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 
 #Functions
     #Information Gain
@@ -12,76 +12,47 @@ from sklearn.preprocessing import LabelEncoder
     #(2 Children per Node)
     #Decision Node
     #Leaf Node
-    #Question Class (type, operator, column, value)
 
+def is_numeric(value):
+    return isinstance(value, int)
 
-def euclideanDistance(X_train, X_test, y_train, i):
-    train_val = X_train
-    test_val = X_test.iloc[i]
-    distances = pd.DataFrame()
-    for x in range(len(test_val)):
-        distances[x] = pow(pow((train_val.iloc[:,x] - test_val[x]), 2),.5)
-    distances = distances.sum(axis=1)
-    distances = pd.concat([distances, y_train], axis=1)
-    distances.columns = ['distance', 'churn']
-    distances = distances.sort_values(by=['distance'])
-    return distances
+class Question:
+    def __init__(self, column, value, operator = '=='):
+        self.column = column
+        self.operator = operator
+        self.value = value
 
-
-def getAccuracy(answers, predictions):
-    correct = 0
-    for p in range(len(predictions)):
-        if answers.iloc[p] == predictions[p]:
-            correct += 1
-    accuracy = (correct / float(len(answers))) * 100.0
-    print('Accuracy: ' + repr(round(accuracy, 2)) + '%')
-
-
-def getClassification(predictions, distances, k):
-    classx = {}
-    for z in range(k + 1):
-        response = distances.iloc[z,1]
-        if response in classx:
-            classx[response] += 1
+    def ask_question(self):
+        if is_numeric(self.value):
+            print('is ' + str(self.column) + ' ' + str(self.operator) + ' ' + str(self.value) + '?')
         else:
-            classx[response] = 0
-    sortedx = sorted(
-        classx.items(),
-        key=operator.itemgetter(1),
-        reverse=True)
-    result = sortedx[0][0]
-    predictions.append(result)
+            print('is ' + str(self.column) + ' equal to ' + str(self.value) + '?') 
 
-
-def loadDataset(filename):
-    le = LabelEncoder()
-    dataset = pd.read_csv(filename)
-    for x in range(len(dataset.columns)):
-        le.fit(dataset.iloc[:,x])
-        dataset.iloc[:,x] = le.transform(dataset.iloc[:,x])
-    y = dataset.iloc[:,-1]
-    X = dataset.drop(['Churn'], axis=1)
-    #Normalize all columns
-    X = (X - X.mean())/X.std()
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
-    print('Train set: ' + repr(len(X_train)), end=', ')
-    print('Test set: ' + repr(len(X_test)))
-    return X_test, y_test, X_train, y_train
+    def answer_question(self, rows):
+        for i, row in rows.iterrows():
+            if is_numeric(self.value):
+                if self.operator == '<':
+                    print(row[self.column] < self.value)
+                else:
+                    print(row[self.column] >= self.value)
+            else:
+                print(row[self.column] == self.value)
 
 if __name__ == "__main__":
-    for t in range(0,11):
-        start = time.time()
-        k = 3
-        predictions = []
-        filename='Telco-Customer-Churn_training_data.csv'
-        #Load the dataset
-        X_test, y_test, X_train, y_train = loadDataset(filename)
-        for i in range(len(X_test)):
-            distances = []
-            #Get distances for each point (Prediction against test)
-            distances = euclideanDistance(X_train, X_test, y_train, i)
-            #Classify each point based on distance
-            getClassification(predictions, distances, k)    
-        #Get accuracy of each classification
-        getAccuracy(y_test, predictions)
-        print("(Time to complete: " + str(round(time.time() - start, 1)) + "s)")
+    training_data = [
+        ['Green', 3, 'Apple'],
+        ['Yellow', 3, 'Apple'],
+        ['Red', 1, 'Grape'],
+        ['Red', 1, 'Grape'],
+        ['Yellow', 3, 'Lemon']]
+
+    training_data = pd.DataFrame(training_data, columns=['Color', 'Diameter', 'Fruit'])
+    columns = training_data.columns
+
+    q1 = Question(columns[0], 'Green')
+    q1.ask_question()
+    q1.answer_question(training_data)
+
+    q2 = Question(columns[1], 3)
+    q2.ask_question()
+    q2.answer_question(training_data)
