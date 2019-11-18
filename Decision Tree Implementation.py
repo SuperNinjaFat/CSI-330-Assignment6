03d150bc55567d90a7fc80c04ddfb554806fb255
tynanpmatthews <tynanpmatthews@gmail.com>
2019-11-18 00:37:44 -0500

    Reword entropy and info gain functions

--- Decision Tree Implementation.py
+++ Decision Tree Implementation.py
@@ -1,4 +1,5 @@
 import csv
+import math
 import time
 import operator
 import numbers
@@ -29,9 +30,10 @@ def get_all_questions(data):
     for col in data.columns:
         for val in data[col].unique():
             q = Question(col, val)
-            q.ask_question()
+            #q.ask_question()
             questions.append(q)
     return questions
+
 
 class DNode:
     def __init__(self, question=None):
@@ -39,28 +41,69 @@ class DNode:
         self.child_true = None
         self.child_false = None
 
+    #???
+    def add_node(self, question):
+        if self.child_true:
+            self.child_true.add_node(question)
+        if self.child_false:
+            self.child_false.add_node(question)
+        else:
+            self.child_true = DNode(question)
+            self.child_false = DNode(question)
+            
 
 def is_numeric(value):
     return isinstance(value, int)
 
-def info_gain(questions):
-    pass
+#???
+def info_gain(data, questions):
+    ent = entropy(data, questions)
 
-def entropy(col):
+    info_gain = []
+    for i in ent:
+        for j in ent[i]:
+            if j != 'total':
+                info_gain.append((j, ent[i]['total']-ent[i][j]))
+    info_gain = sorted(info_gain, key=lambda x: x[1])
+    return info_gain  
+
+#???
+def entropy(data, questions):
     vals = {}
-    
-    for i in col:
-        try:
-            vals[i] += 1
-        except KeyError:
-            vals[i] = 1
-
-    for i in vals.keys():
-        entropy 
-            
-    entropy = 0
-    
+    entropy = {}
+    for j in range(len(data.columns)):
+        vals[data.columns[j]] = {}
+        for i in range(len(data)):
+            try:
+                vals[data.columns[j]][data.iloc[i, j]] += 1
+            except KeyError:
+                vals[data.columns[j]][data.iloc[i, j]] = 1                           
+    for j in range(len(data.columns)):
+        e = 0
+        for i in vals[data.columns[j]]:
+            num = vals[data.columns[j]][i]
+            e += (num/len(data))*math.log2(num/len(data))
+        entropy[data.columns[j]] = {'total': -e}
+    for i in questions:
+        for j in vals.keys():
+            e = 0
+            if i.column == j:
+                for k in vals[j]:
+                    if i.value != k:
+                        num = vals[j][k]
+                        e += (num/len(data))*math.log2(num/len(data))
+                e = e*((len(data)-vals[j][i.value])*.1)
+                entropy[i.column][i.value] = -e                   
     return entropy
+
+#???
+def printTree(node, ind=1):
+    if node.child_true:
+        printTree(node.child_true, ind+1)
+    if node.child_false:
+        printTree(node.child_false, ind+1)
+    else:
+        print('\t'*ind + 'leaf')
 
 if __name__ == "__main__":
     training_data = [
@@ -75,9 +118,27 @@ if __name__ == "__main__":
     columns = training_data.columns
 
     q1 = Question(columns[0], 'Green')
-    q1.ask_question()
-    q1.answer_question(training_data)
+    #q1.ask_question()
+    #q1.answer_question(training_data)
 
     q2 = Question(columns[1], 3)
-    q2.ask_question()
-    q2.answer_question(training_data)
+    #q2.ask_question()
+    #q2.answer_question(training_data)
+
+    #???
+    root = None
+    questions = get_all_questions(training_data)
+    ig = info_gain(training_data, questions)
+    print(ig)
+    for i in ig:
+        for j in questions:
+            if i[0] == j.value:
+                if root:
+                    root.add_node(j)
+                    ig.remove(i)
+                else:
+                    root = DNode(j)
+                    ig.remove(i)
+
+    print('root')
+    printTree(root)
